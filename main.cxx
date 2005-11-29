d195904e18570941aff14b264f7dcc59e9b094d9
makeinu <makeinu>
2005-11-29 16:02:23 +0000

    Check duplicated process.

--- main.cxx
+++ main.cxx
@@ -45,6 +45,112 @@ void showVersion(){
   cout << endl;
 }
 
+
+enum {
+	TARGET_STRING
+};
+
+static GtkTargetEntry targets[]={
+	{"STRING", 0, TARGET_STRING},
+};
+static GdkAtom  atom_kz0;  /* for checking process  */
+static GdkAtom  atom_kz1;  /* for sending arguments */
+static gchar   *arg_text = NULL;
+static gint     instance = -1;
+
+static void cb_selection_get(GtkWidget *widget,
+			     GtkSelectionData *data,
+			     guint info,
+			     guint time,
+			     GtkWidget *window)
+{
+  gchar *text = NULL;
+  gint length = 0;
+
+  if (data->selection == atom_kz0) {
+    text = "Kasumi Selection";
+    length = strlen(text);
+    gtk_selection_convert(window, atom_kz1,
+			  GDK_SELECTION_TYPE_STRING,
+			  GDK_CURRENT_TIME);
+  } else if (data->selection == atom_kz1 && arg_text != NULL) {
+    text = arg_text;
+    arg_text = NULL;
+    length = strlen(text);
+  }
+
+  if (text != NULL) {
+    gtk_selection_data_set_text(data, text, length);
+    if (data->selection == atom_kz1)
+      g_free(text);
+  }
+}
+
+static void cb_selection_received(GtkWidget *widget,
+				  GtkSelectionData *data,
+				  guint time,
+				  gpointer user_data)
+{
+  if (data->selection == atom_kz0) {
+    instance = MAX(data->length, 0);
+  } else if (data->selection == atom_kz1 && data->length > 0) {
+  }
+}
+
+static GtkWidget *check_duplicated_process (int argc, char *argv[])
+{
+  GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gint i, length = 0;
+
+  gtk_widget_realize(window);
+
+  atom_kz0 = gdk_atom_intern("Kasumi InterProcess communication 0",
+			     FALSE);
+  atom_kz1 = gdk_atom_intern("Kasumi InterProcess communication 1",
+			     FALSE);
+
+  gtk_selection_add_targets(window, atom_kz0, targets, 1);
+  gtk_selection_add_targets(window, atom_kz1, targets, 1);
+  g_signal_connect (window, "selection-get",
+		    G_CALLBACK(cb_selection_get), window);
+  g_signal_connect (window, "selection-received",
+		    G_CALLBACK(cb_selection_received), window);
+
+  for (i = 0; i < argc; i++) {
+    gint len;
+
+    len = strlen(argv[i]) * sizeof(gchar);
+    arg_text = (gchar*) g_realloc(arg_text,
+				  length + len + sizeof(gchar));
+    g_memmove(arg_text + length, argv[i], len);
+    length += len;
+    arg_text[length++] = '\n';
+  }
+  if (length > 0) {
+    arg_text[length - 1] = '\0';
+    gtk_selection_owner_set(window, atom_kz1, GDK_CURRENT_TIME);
+  }
+  gtk_selection_convert(window,atom_kz0,
+			GDK_SELECTION_TYPE_STRING,
+			GDK_CURRENT_TIME);
+  while (instance < 0)
+    while (gtk_events_pending())
+      gtk_main_iteration();
+  if (instance > 0) {
+    /* Kasumi process is already exists */
+    while (arg_text != NULL)
+      while (gtk_events_pending())
+	gtk_main_iteration();
+    gtk_widget_destroy(window);
+    return NULL;
+  }
+  g_free(arg_text);
+  arg_text = NULL;
+  gtk_selection_owner_set(window, atom_kz0, GDK_CURRENT_TIME);
+
+  return window;
+}
+
 int main(int argc, char *argv[])
 {
   gtk_init(&argc, &argv);
@@ -54,6 +160,10 @@ int main(int argc, char *argv[])
   bindtextdomain(PACKAGE, LOCALEDIR);
   bind_textdomain_codeset(PACKAGE, "UTF-8");
   textdomain(PACKAGE);
+
+  GtkWidget *server = check_duplicated_process (argc, argv);
+  if (!server)
+    return 0;
 
   try{
     KasumiConfiguration *conf = new KasumiConfiguration(argc, argv);
@@ -75,5 +185,9 @@ int main(int argc, char *argv[])
   }catch(KasumiException e){
     handleException(e);
   }
+
+  gtk_widget_destroy (server);
+
+  return 0;
 }
 
