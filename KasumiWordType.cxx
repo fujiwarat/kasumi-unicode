549c624854a73bfb5b33bfc04d1fe55fe1c22da5
bluedwarf <bluedwarf>
2006-10-14 06:27:33 +0000

    Export some embedded string to po files.

--- KasumiWordType.cxx
+++ KasumiWordType.cxx
@@ -25,7 +25,7 @@
 
 #include "KasumiWordType.hxx"
 #include "KasumiWord.hxx"
-#include <iostream> // for debug
+#include "intl.h"
 
 // static members of KasumiWordType
 WordTypeList KasumiWordType::_lWordTypes;
@@ -33,38 +33,37 @@ int KasumiWordType::comp_seed = 0;
 
 void KasumiWordType::initWordTypeList()
 {
-    KasumiWordType::addNewWordType("名詞", "一般名詞", "#T35");
-    KasumiWordType::addNewWordType("名詞", "人名", "#JNM");
-    KasumiWordType::addNewWordType("名詞", "地名", "#CN");
-    KasumiWordType::addNewWordType("名詞", "会社", "#KK");
-    KasumiWordType::addNewWordType("名詞", "する接続", "#T30");
-    KasumiWordType::addNewWordType("数詞", "数詞", "#NN");
-    KasumiWordType::addNewWordType("形容詞", "ク活用", "#KY");
-    KasumiWordType::addNewWordType("形容詞", "シク活用", "#KYT");
-    KasumiWordType::addNewWordType("形容動詞", "", "#T05");
-    KasumiWordType::addNewWordType("副詞", "", "#F04");
-    KasumiWordType::addNewWordType("感動詞", "", "#CJ");
-    KasumiWordType::addNewWordType("連体詞", "", "#RT");
-    KasumiWordType::addNewWordType("単漢字", "", "#KJ");
-    KasumiWordType::addNewWordType("動詞", "カ行5段", "#K5");
-    KasumiWordType::addNewWordType("動詞", "カ行5段(連用形が名詞)", "#K5r");
-    KasumiWordType::addNewWordType("動詞", "ガ行5段", "#G5");
-    KasumiWordType::addNewWordType("動詞", "ガ行5段(連用形が名詞)", "#G5r");
-    KasumiWordType::addNewWordType("動詞", "サ行5段", "#S5");
-    KasumiWordType::addNewWordType("動詞", "サ行5段(連用形が名詞)", "#S5r");
-    KasumiWordType::addNewWordType("動詞", "タ行5段", "#T5");
-    KasumiWordType::addNewWordType("動詞", "タ行5段(連用形が名詞)", "#T5r");
-    KasumiWordType::addNewWordType("動詞", "ナ行5段", "#N5");
-    KasumiWordType::addNewWordType("動詞", "ナ行5段(連用形が名詞)", "#N5r");
-    KasumiWordType::addNewWordType("動詞", "バ行5段", "#B5");
-    KasumiWordType::addNewWordType("動詞", "バ行5段(連用形が名詞)", "#B5r");
-    KasumiWordType::addNewWordType("動詞", "マ行5段", "#M5");
-    KasumiWordType::addNewWordType("動詞", "マ行5段(連用形が名詞)", "#M5r");
-    KasumiWordType::addNewWordType("動詞", "ラ行5段", "#R5");
-    KasumiWordType::addNewWordType("動詞", "ラ行5段(連用形が名詞)", "#R5r");
-    KasumiWordType::addNewWordType("動詞", "ワ行5段", "#W5");
-    KasumiWordType::addNewWordType("動詞", "ワ行5段(連用形が名詞)", "#W5r");
-
+    KasumiWordType::addNewWordType(_("Noun"), _("General Noun"), "#T35");
+    KasumiWordType::addNewWordType(_("Noun"), _("Name of people"), "#JNM");
+    KasumiWordType::addNewWordType(_("Noun"), _("Geographic Name"), "#CN");
+    KasumiWordType::addNewWordType(_("Noun"), _("Corporate Name"), "#KK");
+    KasumiWordType::addNewWordType(_("Noun"), _("SURU Connection"), "#T30");
+    KasumiWordType::addNewWordType(_("Numeral"), "", "#NN");
+    KasumiWordType::addNewWordType(_("Adjective (Keiyoushi)"), _("KU Conjugation"), "#KY");
+    KasumiWordType::addNewWordType(_("Adjective (Keiyoushi)"), _("SIKU Conjugation"), "#KYT");
+    KasumiWordType::addNewWordType(_("Adjective (Keiyoudoushi)"), "", "#T05");
+    KasumiWordType::addNewWordType(_("Adverb"), "", "#F04");
+    KasumiWordType::addNewWordType(_("Interjection"), "", "#CJ");
+    KasumiWordType::addNewWordType(_("Adnominal adjunct"), "", "#RT");
+    KasumiWordType::addNewWordType(_("Single Kanji Character"), "", "#KJ");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ka 5"), "#K5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ga 5"), "#G5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Sa 5"), "#S5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ta 5"), "#T5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Na 5"), "#N5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ba 5"), "#B5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ma 5"), "#M5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Ra 5"), "#R5");
+    KasumiWordType::addNewWordType(_("Verb"), _("Wa 5"), "#W5");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ka 5"), "#K5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ga 5"), "#G5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Sa 5"), "#S5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ta 5"), "#T5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Na 5"), "#N5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ba 5"), "#B5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ma 5"), "#M5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Ra 5"), "#R5r");
+    KasumiWordType::addNewWordType(_("Verb*"), _("Wa 5"), "#W5r");
 }
 
 KasumiWordType::KasumiWordType(string sPos, string sCategory, string sCannaTab)
