3b6d13ff73e55ea53de07ec546a64950db76d7e0
fujiwarat <takao.fujiwara1@gmail.com>
2021-07-14 11:51:05 +0900

    Move custom CFLAGS from configure.in to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-autopoint
-aclocal -I m4
-autoheader
-automake --add-missing --copy --include-deps
-autoconf
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
+
+cd "$srcdir"
+
+CFLAGS=${CFLAGS-"-std=c++14 -Wall -Wformat -Werror=format-security"}
+CPPFLAGS=${CPPFLAGS-"-std=c++14 -Wall -Wformat -Werror=format-security"}
+
+autoreconf --verbose --force --install || exit 1
+CFLAGS="$CFLAGS" CPPFLAGS="$CPPFLAGS" $srcdir/configure "$@" || exit 1
